4f82d9283f67674a5cd5b5fc4f7bc827e7b17659
cedlemo <cedlemo@gmx.com>
2015-05-14 12:23:41 +0200

    add macros convertion for string and begin for lua array value

--- macros.h
+++ macros.h
@@ -6,7 +6,8 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
-
+#include "lua.h"
+#include "lauxlib.h" 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 #define SENTINEL(M, ...) printf("[SENTINEL] (%s:%s:%d: errno: %s) " M "\n",__FILE__, __FUNCTION__, __LINE__, clean_errno(), ##__VA_ARGS__)
 #define LOG_ERR(M, ...) fprintf(stderr,  "[ERROR] (%s:%s:%d: errno: %s) " M "\n", __FILE__, __FUNCTION__, __LINE__, clean_errno(), ##__VA_ARGS__)
@@ -20,4 +21,22 @@
 /*****************/
 #define CARRAY_LEN(A)   (sizeof(A) / sizeof((A)[0]));
 
+/**********************/
+/*Lua Stack to C value*/
+/**********************/
+//Lua string to c string pointer
+#define L_2_CSTRING(L, i)\
+lua_isstring(L,i)? lua_tostring(L, i) : NULL
+//Lua array to c array of string pointers
+#define L_2_CSTRINGARRAY(L, i)\
+if(lua_istable(L, i))\
+{\
+  int len = luaL_len(L, i);\
+  SENTINEL("size %d",len);\
+  if(len)\
+  {\
+  }\
+}\
+else\
+  SENTINEL("Not a table");
 #endif //MACROS_H
