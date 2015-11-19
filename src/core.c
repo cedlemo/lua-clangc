c8031dcb3ddb52c224ee1cb6852ce5d1a0102650
cedlemo <cedlemo@gmx.com>
2015-11-19 19:09:44 +0100

    Remove comments

--- src/core.c
+++ src/core.c
@@ -33,52 +33,40 @@ static const struct luaL_Reg indexlib_instances_methods[] = {
   {NULL, NULL}
 };
 int luaopen_clangc_core(lua_State *L) {
-//  SENTINEL("### luaopen %d ###", lua_gettop(L));
-  lua_newtable(L); //create the main module table
-//  SENTINEL("### newtable %d ###", lua_gettop(L));
+  lua_newtable(L);
 
 /*
 * Index Class
 */
-  /*Metatable for Index class*/
   luaL_newmetatable(L, "Clangc.Index_mt");
-  SENTINEL("### newtmetatable Index_mt %d ###", lua_gettop(L));
-  /*set metatable __gc field*/
   lua_pushcfunction(L, index__gc);
-  lua_setfield(L, -2, "__gc"); //pop the last element of the stack to the table at stack index
-  SENTINEL("### set Index_mt field __gc %d ###", lua_gettop(L));
-  /*set metatble __index field to metatable */
-  lua_pushvalue(L, -1); //duplicate the metatable
-  lua_setfield(L, -2, "__index"); //store the duplicate in the __index field
-  SENTINEL("### set Index_mt field __index %d ###", lua_gettop(L));
-  luaL_setfuncs(L, indexlib_instances_methods, 0); //store the fns in the metatable  
-  SENTINEL("### store fns in Index_mt %d ###", lua_gettop(L));
-  /*Create the Index table used as Constructor*/
+  lua_setfield(L, -2, "__gc");
+  lua_pushvalue(L, -1);
+  lua_setfield(L, -2, "__index");
+  luaL_setfuncs(L, indexlib_instances_methods, 0); 
   luaL_newlib(L, indexlib_classes_functions);
-  SENTINEL("###Create the index table as Constructor %d ###", lua_gettop(L));
   lua_setfield(L, -2, "Index");
-  SENTINEL("### Store the Index table in the main table %d ###", lua_gettop(L));
-  /*Manage TranslationUnit class */
+
+/*
+* TranslationUnit class
+*/
   /*Create the metatable for TranslationUnit object*/
-//  luaL_newmetatable(L, "Clangc.TranslationUnit_mt");
+  //luaL_newmetatable(L, "Clangc.TranslationUnit_mt");
   /*set metatable __gc field*/
-//  lua_pushcfunction(L, translationunit__gc);
+  //lua_pushcfunction(L, translationunit__gc);
   /* When TranslationUnit methods will have to be created
   lua_pushvalue(L, -1); //duplicate the metatable
   lua_setfield(L, -2, "__index"); //store the duplicate in the __index field
   luaL_setfuncs(L, translationunit_instances_methods, 0); //store the fns in the metatable
-*/
+  */
 
 /*
 * Create Clangc module functions
 */
   lua_pushcfunction(L, clangc_get_version);
-  SENTINEL("### Store the version fn int the stack %d ###", lua_gettop(L));
   lua_setfield(L, -2, "version");
-  SENTINEL("### Store the version fn in the main table %d ###", lua_gettop(L));
   lua_pushcfunction(L, clangc_get_default_diagnostic_display_options);
   lua_setfield(L, -2, "default_diagnostic_display_options");
-  SENTINEL("### Store the constants table in the main table%d ###", lua_gettop(L));
 
-  return 1; //return 1 in order to return the table
+  return 1;
 }
