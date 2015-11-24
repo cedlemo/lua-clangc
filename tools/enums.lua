37ac1485d714bb225d0a6c5a2951ec73a507e7f0
cedlemo <cedlemo@gmx.com>
2015-11-24 18:42:46 +0100

    Use toolbox module and simplify

--- tools/enums.lua
+++ tools/enums.lua
@@ -1,38 +1,11 @@
 #!/usr/bin/env lua
 
-local truckboris = require('truckboris')
-local lfs = require('lfs')
+local toolbox = require('toolbox')
 
-local function split(str, pat)
-  local t = {}  -- NOTE: use {n = 0} in Lua-5.0
-  local fpat = "(.-)" .. pat
-  local last_end = 1
-  local s, e, cap = string.find(str,fpat, 1)
-  while s do
-    if s ~= 1 or cap ~= "" then
-      table.insert(t,cap)
-    end
-    last_end = e+1
-    s, e, cap = string.find(str,fpat, last_end)
-  end
-  if last_end <= #str then
-    cap = string.sub(str,last_end)
-    table.insert(t, cap)
-  end
-  return t
-end
-
-local function exec_out(cmd)
-  local handle = io.popen(cmd)
-  local result = string.gsub(handle:read("*a"), "\n", "")
-  handle:close()
-  return result
-end
-
-local gcc_lib_base = '/usr/lib/gcc/' .. exec_out('llvm-config --host-target')
+local gcc_lib_base = '/usr/lib/gcc/' .. toolbox.exec_out('llvm-config --host-target')
 
 local last = ''
-for dir in lfs.dir(gcc_lib_base) do
+for dir in toolbox.lfs.dir(gcc_lib_base) do
   if not dir:match('^..?$') then
     if dir > last then
       last = dir
@@ -47,7 +20,7 @@ headers_paths[3] = '/usr/include/clang-c'
 
 clangc_source = '/usr/include/clang-c/Index.h'
 
-local parser = truckboris.HeaderParser()
+local parser = toolbox.truckboris.HeaderParser()
 parser:add_source_file(clangc_source)
 for _,h in ipairs(headers_paths) do
   parser:add_search_path(h)
@@ -65,17 +38,6 @@ for i = 1, parser:enums_num(), 1 do
   enums[i] = parser:get_nth_enum(i - 1)
 end
 
-local function to_snake_case(a_string)
-  if not a_string:match("[A-Z]") or not a_string:match("[a-z]") then
-    return a_string
-  else
-    s = a_string:gsub("([a-z]+)([A-Z][a-z]%a*)","%1_%2")
-    s = a_string:gsub("([A-Z]+)([A-Z][a-z]%a*)","%1_%2")
-    s = s:gsub("([a-z]+)([A-Z]+)","%1_%2")
-    return s
-  end
-end
-
 local function rename_module_table(enum)
   local name = enum:name() == "" and enum:typedef_name() or enum:name()
   return name:gsub('^CX_?','') 
@@ -83,8 +45,7 @@ end
 
 local function rename_enum_constant(constant_name)
   local name = constant_name:gsub("^([^_]+_)(.*)$","%2")
-  name = to_snake_case(name)
-  return name:upper()
+  return toolbox.to_screaming_snake_case(name)
 end
 
 local function create_module_constant(enum)
